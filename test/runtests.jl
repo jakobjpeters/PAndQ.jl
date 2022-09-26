8826e738b4ece3027b7d6a9bf95a6b9d2f4cc693
Jakob Peters <jakobj.peters@gmail.com>
2022-09-25 22:15:43 -0700

    Use 'main' function in 'runtests.jl'

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,25 +1,29 @@
 
 using Logic
 
-boolean_operators = [¬, ∧, ⊼, ⊽, ∨, ⊻, →, ←, ↔]
+function main()
+    boolean_operators = [¬, ∧, ⊼, ⊽, ∨, ⊻, →, ←, ↔]
 
-for f in boolean_operators
-    println("Truth table of: ", f)
-    println(truth_table(f))
+    for f in boolean_operators
+        println("Truth table of: ", f)
+        println(truth_table(f))
+        println()
+    end
+
+    p = Proposition("Propositional logic is fun")
+    q = Proposition("Modal logic is hard")
+    r = ¬Proposition("This code has bugs")
+    s = q → r
+
+    println(q ∧ r)
     println()
+
+    println("s ≡ ", s())
+    println("p ∨ s ≡ ", (p ∨ s)())
+    println()
+
+    println("length(s): ", length(s))
+    println("depth(s): ", depth(s))
 end
 
-p = Proposition("Propositional logic is fun")
-q = Proposition("Modal logic is hard")
-r = ¬Proposition("This code has bugs")
-s = q → r
-
-println(q ∧ r)
-println()
-
-println("s ≡ ", s())
-println("p ∨ s ≡ ", (p ∨ s)())
-println()
-
-println("length(s): ", length(s))
-println("depth(s): ", depth(s))
+main()
